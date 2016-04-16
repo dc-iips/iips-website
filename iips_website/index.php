5aa1aa702c2bd48c83a112c4d7a07074752053f9
Vikas Chouhan <aboutvikaschouhan@gmail.com>
2016-04-16 17:17:28 +0530

    Update slider

--- iips_website/index.php
+++ iips_website/index.php
@@ -40,6 +40,13 @@
       		<div class="carousel-caption">
       		</div>
     	</div>
+
+      <div class="item">
+          <img src="images/campus_life.jpg" alt="...">
+          <div class="carousel-caption">
+          </div>
+      </div>
+
   	</div>
 
   	<!-- Controls -->
