091c30c72a4d8919bba540d6f49b5a2e84b9314d
rupamwadia <wadia.rupam@gmail.com>
2016-04-18 13:32:36 +0530

    Some minor changes

--- iips_website/greviances.php
+++ iips_website/greviances.php
@@ -22,7 +22,7 @@
   <div class="row">
     <div class="col-md-3" id="sidebar">
       <ul class="nav nav-stacked" role="tablist">
-        <li class="navbar-brand"><b>IQAC</b></li>
+        <li class="navbar-brand"><b>Greviances</b></li>
         <li role="presentation" id="colm" class="active"><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Ragging Complaint</a></li>
         <li role="presentation" id="colm"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Sexual Harrasment</a></li>
         <li role="presentation" id="colm"><a href="#tab3" aria-controls="tab3" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Student Complaint</a></li>
@@ -32,15 +32,15 @@
     <div class="col-md-9">
       <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="tab1">
-            <h3 class="text-center">Ragging Complaint</h3>
+            <h3 class="text-center"><b>Ragging Complaint</b></h3>
         </div>
 
         <div role="tabpanel" class="tab-pane" id="tab2">
-            <h3 class="text-center">Sexual Harrasment</h3>     
+            <h3 class="text-center"><b>Sexual Harrasment</b></h3>     
         </div>
 
         <div role="tabpanel" class="tab-pane" id="tab3">
-            <h3 class="text-center">Student Complaint</h3>
+            <h3 class="text-center"><b>Student Complaint</b></h3>
         </div>
       </div>
   <br><br>
