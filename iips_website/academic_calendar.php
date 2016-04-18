d5338888c523cc8dd16123d3704ceb990e379278
Rahul-sinha <007rahul.sinha@gmail.com>
2016-04-18 13:58:32 +0530

    Local Holiday tab add to page

--- iips_website/academic_calendar.php
+++ iips_website/academic_calendar.php
@@ -22,6 +22,7 @@
     <div class="col-md-3" id="sidebar">
       <ul class="nav nav-stacked" role="tablist">
         <li role="presentation" id="colm" class="active" ><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Academic Calendar</a></li>   
+        <li role="presentation" id="colm" ><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp; Local Holidays </a></li>   
       </ul>
     </div>
 
@@ -33,6 +34,17 @@
                 <div class="col-md-1"></div>
                 <div class="col-md-10">
                     <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;height=500&amp;wkst=1&amp;hl=en&amp;bgcolor=%2366cccc&amp;src=a5miku0t4g7j0bjda632f7rba0%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Asia%2FCalcutta" style="border:solid 1px #777" width="100%" height="500" frameborder="0" scrolling="no"></iframe>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+        <div role="tabpanel" class="tab-pane active" id="tab2">
+            <h3 class="text-center">Local Holidays</h3> <br/> <br/>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                 <div class="col-md-4 col-sm-6 col-xs-6"> <a href="pdf_server.php?file=static/pdf/LocalHolidayNotice2016.pdf"> Click here </a> to download
+                 </div>
                 </div>
                 <div class="col-md-1"></div>
             </div>
