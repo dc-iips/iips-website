1a7b6db670b653fb070a03aa0fbff816e0ceaed5
rupamwadia <wadia.rupam@gmail.com>
2016-04-18 16:01:48 +0530

    minor changes

--- iips_website/results.php
+++ iips_website/results.php
@@ -28,7 +28,7 @@
         <li role="presentation" id="colm"><a href="#tab4" aria-controls="tab4" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;MBA(MS-2 years)</a></li>
         <li role="presentation" id="colm"><a href="#tab5" aria-controls="tab5" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;MBA(TA-2 years)</a></li>
         <li role="presentation" id="colm"><a href="#tab6" aria-controls="tab6" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;MBA(APR-2 years)</a></li>
-        <li role="presentation" id="colm"><a href="#tab7" aria-controls="tab7" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;B.COM(Hons.)</a></li>
+        <li role="presentation" id="colm"><a href="#tab7" aria-controls="tab7" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;B.COM(Hons-2 years)</a></li>
       </ul>
     </div>
 
@@ -231,7 +231,7 @@
     </div>
 
     <div role="tabpane7" class="tab-pane" id="tab7">
-    <h3 class="text-center"> <b>B.com(Hons.)</b> </h3>  
+    <h3 class="text-center"> <b>B.com(Hons-2 years)</b> </h3>  
         <table align="center" border="1px" class="table" >
                       <tr style="color:#FFFFFF; background-color:#333333"> <td>Semesters</td> <td>Download Links</td>  </tr>
                         
