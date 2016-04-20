5a5c12dfe11eeb76220169f2109c583b04d47ec6
rupamwadia <wadia.rupam@gmail.com>
2016-04-20 16:28:12 +0530

    Add my links in developer page

--- iips_website/developers.php
+++ iips_website/developers.php
@@ -80,10 +80,10 @@
                 <img class="img-circle img-responsive" id="centralized" src="images/rupam.jpg" alt="Rupam Wadia">
                 <h3>Rupam Wadia<br><small>Front-End Developer</small></h3>
                 <p>
-                  <a href="" target="__blank"><i class="fa fa-twitter-square fa-2x"></i></a>
-                  <a href="" target="__blank"><i class="fa fa-facebook-square fa-2x"></i></a>
-                  <a href="" target="__blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
-                  <a href="" target="__blank"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                  <a href="https://twitter.com/RupamWadia" target="__blank"><i class="fa fa-twitter-square fa-2x"></i></a>
+                  <a href="https://www.facebook.com/rupam.wadia.3" target="__blank"><i class="fa fa-facebook-square fa-2x"></i></a>
+                  <a href="https://www.linkedin.com/in/rupam-wadia-a2303991?trk=nav_responsive_tab_profile_pic" target="__blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                  <a href="https://www.linkedin.com/in/rupam-wadia-a2303991?trk=nav_responsive_tab_profile_pic" target="__blank"><i class="fa fa-google-plus-square fa-2x"></i></a>
                 </p>
                 </div>
                 <div class="col-lg-4 col-sm-6 text-center">
