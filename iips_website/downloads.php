e8c42c3324735ed7b340a22f291ea3852ad99a5f
rupamwadia <wadia.rupam@gmail.com>
2016-04-20 15:57:08 +0530

    Make new download page

---
+++ iips_website/downloads.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="shortcut icon" href="images/favicon.jpg">
+    <title>Downloads | International Institute of Professional Studies, Devi Ahilya Vishwavidhalay</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/newfile.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="yamm/yamm.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+<?php include 'header.php'; ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-3" id="sidebar">
+      <ul class="nav nav-stacked" role="tablist">
+        <li class="navbar-brand"> <b>Downloads</b> </li>
+        <li role="presentation" id="colm" class="active" ><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Downloads</a></li>
+      </ul>
+    </div>
+
+    <div class="col-md-9">
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="tab1">
+                <h3 class="text-center"><b>Download</b></h3>
+            <!--Collapse start-->
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab">
+      <h4 class="panel-title">
+        <a href="syllabus.php">
+            Syllabus
+        </a>
+      </h4>
+    </div>
+
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab">
+      <h4 class="panel-title">
+        <a href="results.php">
+            Results
+        </a>
+      </h4>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab">
+      <h4 class="panel-title">
+        <a href="academic_calendar.php">
+          Academic Calendar
+        </a>
+      </h4>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFour">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+            Time Table
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="panel-body">
+        <ul>
+            <a href=""> MCA  <br>
+            <a href=""> M. Tech <br>
+            <a href=""> Teacher Wise 
+        </ul>   
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFive">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+            Format
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+        <b>Format</b>
+            <a href=""> Indent Form <br>
+            <a href=""> Casual Leave Application <br>
+            <a href=""> Student Appraisal Form 
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingSix">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+            Faculty CV's
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
+      <div class="panel-body">
+        <a href="">Download all Faculties CV's Here</a>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingSeven">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+            Course PPT by Subject Teacher
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
+      <div class="panel-body">
+        <a herf="">Download all Faculties PPT's Here</a>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  
+</div>
+<!--Collapse end-->             
+
+
+
+            </div>
+        </div>
+        <br><br>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<?php include 'footer.php'; ?>
