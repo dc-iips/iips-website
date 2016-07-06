5e81e077d806514f5d7bcaa72a22ce3619d00ddd
Vikas Chouhan <aboutvikaschouhan@gmail.com>
2016-07-06 15:56:23 +0530

    Open the pdf then download it

--- iips_website/academic_calendar.php
+++ iips_website/academic_calendar.php
@@ -44,7 +44,8 @@
             <div class="row">
                 <div class="col-md-1"></div>
                 <div class="col-md-10">
-                 <div class="col-md-4 col-sm-6 col-xs-6"> <a href="pdf_server.php?file=static/pdf/LocalHolidayNotice2016.pdf"> Click here </a> to download
+                 <div class="col-md-4 col-sm-6 col-xs-6"> <a href="pdf\LocalHolidayNotice2016.pdf"> <span class="fa fa-hand-o-right"></span>
+                  Click here </a> to download
                  </div>
                 </div>
                 <div class="col-md-1"></div>
