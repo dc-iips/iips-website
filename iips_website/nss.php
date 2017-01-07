254ec096d7eaf2b81c7ce3c0ac20eda1ee8a6773
rupamwadia <aboutvikaschouhan@gmail.com>
2017-01-07 15:40:35 +0530

    add nss page

---
+++ iips_website/nss.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="shortcut icon" href="images/favicon.jpg">
+    <title>Staff Profile | International Institute of Professional Studies, Devi Ahilya Vishwavidhalay</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/newfile.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="yamm/yamm.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+<?php include 'header.php';?>
+<div class="container">
+  <div class="row , minlength">
+    <div class="col-md-3" id="sidebar">
+      <ul class="nav nav-stacked" role="tablist">
+        <li class="navbar-brand"> <b>NSS</b> </li>
+        <li role="presentation" id="colm" class="active" ><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;NSS Volunteer Registration</a></li>
+        <li role="presentation" id="colm"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Registration Form</a></li>
+        <li role="presentation" id="colm"><a href="#tab3" aria-controls="tab3" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Notice</a></li>       
+      </ul>
+    </div>
+
+    <div class="col-md-9">
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="tab1">
+          <h3 class="text-center"><b>Online NSS Volunteer Registration</b></h3>
+             <a href="https://docs.google.com/forms/d/e/1FAIpQLSepVI8cxQcwEt748xXzSC8VZqY2ktYQW6opwm1TWWwNi0t9xw/viewform?c=0&w=1">Online Form for the NSS Volunteer Registration 
+             <br> <span class="fa fa-hand-o-right"></span>Click here to fill the form</a>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="tab2">
+          <h3 class="text-center"> <b>Registration Form</b> </h3>  
+             <a href="https://drive.google.com/open?id=0B7MWn6sZED3XTEc1ZVNHUVpsMGs">NSS Volunteers Registration Form 
+              <br><span class="fa fa-hand-o-right"></span>Download</a>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="tab3">
+          <h3 class="text-center"> <b>Notice</b> </h3>  
+            <a href="https://drive.google.com/open?id=0B7MWn6sZED3XN0FQel9EekgzcGc">Notice: NSS Volunteers Registration re-opens
+              <br><span class="fa fa-hand-o-right"></span> Download</a>
+        </div>
+      </div>
+    </div><!--Col-md-9 End> -->
+
+  </div>
+</div>
+<?php include 'footer.php'; ?>
