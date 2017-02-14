34fd8d34879aacd66b022969dfaed7942d3f21af
Vikas Chouhan <Vikas Chouhan>
2017-02-14 10:50:55 +0530

    Add Cocurriculam and logout functionality

---
+++ iips_website/cocurricular.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="shortcut icon" href="images/favicon.jpg">
+    <title>History | International Institute of Professional Studies, Devi Ahilya Vishwavidhalay</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/newfile.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="yamm/yamm.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+<?php include 'header.php'; ?>
+<div class="container">
+  <div class="row , minlength">
+    <div class="col-md-3" id="sidebar">
+      <ul class="nav nav-stacked" role="tablist">
+        <li class="navbar-brand"> <b></b> </li>
+        <li role="presentation" id="colm" class="active" ><a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Co-Curricular, Extension,Professional Development Related Activity</a></li>
+        <li role="presentation" id="colm"><a href="#tab2" aria-controls="tab2" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Contribution to Corporate Life and Management of the Institution</a></li>
+        <li role="presentation" id="colm"><a href="#tab3" aria-controls="tab3" role="tab" data-toggle="tab"><span class="fa fa-hand-o-right"></span>&nbsp;Professional Development Activities</a></li>
+      </ul>
+    </div>
+    <div class="col-md-9">
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="tab1">
+                <h3 class="text-center"><b>Co-Curricular, Extension Related Activity</b></h3>
+                vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+            </div>
+            
+            <div role="tabpanel" class="tab-pane" id="tab2">
+                <h3 class="text-center"><b>Contribution to Corporate Life and Management of the Institution</b> </h3> 
+                iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii    
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="tab3">
+                <h3 class="text-center"><b>Professional Development Activities</b> </h3> 
+                kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk    
+            </div>  
+        </div>
+        <br><br>
+    </div><!--Col-md-9 End> -->
+  </div>
+</div>
+<?php include 'footer.php'; ?>
